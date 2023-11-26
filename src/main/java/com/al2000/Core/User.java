f3d44b51226e101a453cf1474521c0f9382e305e
NewYards <43386297+NewYards@users.noreply.github.com>
2023-11-26 14:20:39 +0100

    Delete Functional

--- src/main/java/com/al2000/Core/User.java
+++
@@ -1,5 +0,0 @@
-package com.al2000.Core;
-
-public abstract class User {
-    protected int id;
-}
--- src/main/java/com/al2000/Core/UserFactory.java
+++
@@ -1,8 +0,0 @@
-package com.al2000.Core;
-import com.al2000.Data.data.Utilisateur;
-
-public class UserFactory {
-    public Subscriber createSubscriber (Utilisateur u) {
-        return new Subscriber(u.getId(), u.getNom(), 0);
-    }
-}
