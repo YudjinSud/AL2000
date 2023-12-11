9bb2ae00488bf96b1245b53f4db81879fa0e47e1
Lukasz Matyasik <lukasz.matyasik@inesocompany.com>
2023-12-11 17:38:15 +0100

    Added: FacadeData

---
+++ src/main/java/com/al2000/Core/FacadeDataFct.java
@@ -0,0 +1,46 @@
+package com.al2000.Core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FacadeDataFct {
+
+
+    public void addSub(Subscriber Subscriber) {
+       // database.addUser(Subscriber);
+    }
+
+    public void updateSub(Subscriber Subscriber) {
+        // loop in subcribers to find subscriber
+        //database.subscriber.set(subscriber);
+    }
+    public List<Subscribers> getSubs() {
+        //return database.getSubs();
+    }
+
+    public List<Movie> getMovies() {
+        //return database.getMovies();
+    }
+
+    public List<SubscriberHisotory> getHistory() {
+        //return database.getSubscriberHistories();
+    }
+
+
+
+
+    // public void addSubHistory(Subscriber subscriptionHistory) {
+    //     database.addSubscriptionHistory(subscriptionHistory);
+    //     }
+
+    
+    // public void addRestrictedCat(RestrictedCategory restrictedCategory) {
+    //  database.addRestrictedCategory(restrictedCategory);
+    // }
+    
+    // public void delRestrictedCat(RestrictedCategory restrictedCategory) {
+    // database.deleteRestrictedCategory(restrictedCategory);
+    // }
+}
